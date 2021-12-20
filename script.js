518b89c9fb123fdabb2bc2823d6c28635a064fcb
DESKTOP-LMUEBM4\AA3 <shalom.webdev@gmail.com>
2021-12-20 01:43:53 +0100

    First code for the project

---
+++ script.js
@@ -0,0 +1,28 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.querySelectorAll(".slidesContainer").forEach(slidesContainer =>
+    slidesContainer.addEventListener("wheel", (evt) => {
+        evt.preventDefault();
+        slidesContainer.scrollLeft += evt.deltaY;
+    }))
+
+
+function resize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('resize', resize);
+window.addEventListener('load', resize);
